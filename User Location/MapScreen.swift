019299a388852d2a213ad6322497ecc4f10e80bb
Charles Martin Reed <charlesmartinreed@icloud.com>
2018-12-18 16:01:13 -0600

    Add location pin and location updating.

--- User Location/MapScreen.swift
+++ User Location/MapScreen.swift
@@ -13,11 +13,13 @@ import CoreLocation //because we're using user location
 class MapScreen: UIViewController {
 
     //MARK:- @IBOutlets
-    @IBOutlet weak var mapView: MKMapView! //strong reference, private to this file
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var addressLabel: UILabel!
     
     //MARK:- Instance properties
     let locationManager = CLLocationManager()
     let regionInMeters: Double = 10000
+    var previousLocation: CLLocation?
     
     
     override func viewDidLoad() {
@@ -52,11 +54,7 @@ class MapScreen: UIViewController {
     func checkLocationAuthorization() {
         switch CLLocationManager.authorizationStatus() {
         case .authorizedWhenInUse: //when app open, in foreground
-            mapView.showsUserLocation = true //this gives our blue location dot
-            centerViewOnUserLocation()
-            //update when user moves - fires off didUpdateLocations() protocol method
-            locationManager.startUpdatingLocation()
-            break
+            startTrackingUserLocation()
         case .denied: //user denied, show alert on how to turn on permissions
             break
         case .notDetermined: //user hasn't picked allow or not allowed
@@ -68,7 +66,23 @@ class MapScreen: UIViewController {
         }
     }
     
-    //MARK:- Placing the user location region in center of view
+    func startTrackingUserLocation() {
+        mapView.showsUserLocation = true //this gives our blue location dot
+        centerViewOnUserLocation()
+        //update when user moves - fires off didUpdateLocations() protocol method
+        locationManager.startUpdatingLocation()
+        previousLocation = getCenterLocation(for: mapView)
+    }
+    
+    //MARK:- Center view methods
+    func getCenterLocation(for mapview: MKMapView) -> CLLocation {
+        //grab the coordinates for the point at the center of the mapView
+        let latitude = mapview.centerCoordinate.latitude
+        let longitude = mapview.centerCoordinate.longitude
+        
+        return CLLocation(latitude: latitude, longitude: longitude)
+    }
+    
     func centerViewOnUserLocation() {
         //location returns optionally
         if let location = locationManager.location?.coordinate {
@@ -80,21 +94,55 @@ class MapScreen: UIViewController {
 
 }
 
-
-
-//MARK:- Delegate conformation via extension
+//MARK:- Delegate methods implemented vai extensions
 extension MapScreen: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         
-        guard let locations = locations.last else { return } //again, locations return optionally
-        
-        let center = CLLocationCoordinate2D(latitude: locations.coordinate.latitude, longitude: locations.coordinate.longitude)
-        let region = MKCoordinateRegion(center: center, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
-        mapView.setRegion(region, animated: true)
+//        guard let locations = locations.last else { return } //again, locations return optionally
+//
+//        let center = CLLocationCoordinate2D(latitude: locations.coordinate.latitude, longitude: locations.coordinate.longitude)
+//        let region = MKCoordinateRegion(center: center, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+//        mapView.setRegion(region, animated: true)
     }
     
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
 
         checkLocationAuthorization()
+    }
+}
+
+extension MapScreen: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        //center of map is where the pin exists, when it changes, use the new coordinate to reverse geolocate an address and then put that onto labels
+        let center = getCenterLocation(for: mapView)
+        let geocoder = CLGeocoder()
+        
+        //there's a max call limit for geolocation, so we need to only need to update when significant movement occurs; over 50 meters
+        guard let previousLocation = self.previousLocation else { return }
+        
+        guard center.distance(from: previousLocation) > 50 else { return }
+        self.previousLocation = center
+        
+        geocoder.reverseGeocodeLocation(center) { [weak self] (placemarks, error) in
+            guard let self = self else { return } //weak self to avoid strong reference cycles
+            
+            if let _ = error {
+                //show alert informing user of the error
+                return
+            }
+            
+            guard let placemark = placemarks?.first else {
+                //show alert for error
+                return
+            }
+            
+            //setup address - using nil coalescing sine this can return nil
+            let streetNumber = placemark.subThoroughfare ?? ""
+            let streetName = placemark.thoroughfare ?? ""
+            
+            DispatchQueue.main.async {
+                self.addressLabel.text = "\(streetNumber) \(streetName)"
+            }
+        }
     }
 }
